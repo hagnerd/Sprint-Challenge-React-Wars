290bab56a480e88ca4cb929a6404325e7b7f1e6d
Matt Hagner <web@matthagner.com>
2019-06-08 13:01:54 -0500

    Add logic to app to handle pagination

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import StarWarsCharacterList from "./components/StarWarsCharacterList";
+import Pagination from "./components/Pagination";
 import "./App.css";
 
 class App extends Component {
@@ -7,12 +8,15 @@ class App extends Component {
     super();
     this.state = {
       starwarsChars: [],
-      isLoading: true
+      isLoading: true,
+      currentLink: "https://swapi.co/api/people/",
+      prevLink: null,
+      nextLink: null
     };
   }
 
   componentDidMount() {
-    this.getCharacters("https://swapi.co/api/people/");
+    this.getCharacters(this.state.currentLink);
   }
 
   getCharacters = URL => {
@@ -25,22 +29,43 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results, isLoading: false });
+        this.setState({
+          starwarsChars: data.results,
+          isLoading: false,
+          prevLink: data.previous,
+          nextLink: data.next
+        });
       })
       .catch(err => {
         throw new Error(err);
       });
   };
 
+  handleNextLinkClick = () => {
+    this.getCharacters(this.state.nextLink);
+  };
+
+  handlePrevLinkClick = () => {
+    this.getCharacters(this.state.prevLink);
+  };
+
   render() {
-    const { starwarsChars, isLoading } = this.state;
+    const { starwarsChars, isLoading, nextLink, prevLink } = this.state;
 
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+
         {isLoading ? null : (
           <StarWarsCharacterList characters={starwarsChars} />
         )}
+
+        <Pagination
+          handlePrevClick={this.handlePrevLinkClick}
+          handleNextClick={this.handleNextLinkClick}
+          isPrevLinkDisabled={prevLink === null || isLoading}
+          isNextLinkDisabled={nextLink === null || isLoading}
+        />
       </div>
     );
   }
