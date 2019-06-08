7f47a9a2bf9011d9fdd003959cbd0d5c636da935
Matt Hagner <web@matthagner.com>
2019-06-08 12:17:38 -0500

    Change component requirements

--- starwars/src/components/StarWarsCharacterList.js
+++ starwars/src/components/StarWarsCharacterList.js
@@ -5,16 +5,31 @@ import PropTypes from "prop-types";
 export default function StarWarsCharacterList({ characters }) {
   return (
     <ul>
-      {characters.map(character => (
-        <StarWarsCharacter key={character.url} {...character} />
-      ))}
+      {characters.map(character => {
+        console.log(character["birth_year"]);
+
+        return (
+          <StarWarsCharacter
+            key={character.url}
+            name={character.name}
+            height={character.height}
+            gender={character.gender}
+            birthYear={character["birth_year"]}
+          />
+        );
+      })}
     </ul>
   );
 }
 
 StarWarsCharacterList.propTypes = {
-  characters: PropTypes.shape({
-    ...StarWarsCharacter.propTypes,
-    url: PropTypes.string
-  })
+  characters: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      birth_year: PropTypes.string.isRequired,
+      height: PropTypes.string.isRequired,
+      gender: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired
+    })
+  )
 };
