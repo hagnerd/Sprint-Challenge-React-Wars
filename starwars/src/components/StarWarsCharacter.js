afca3776eaa9b58923d49371d81af65a65c77d7d
Matt Hagner <web@matthagner.com>
2019-06-08 12:33:37 -0500

    Add classes for styling

--- starwars/src/components/StarWarsCharacter.js
+++ starwars/src/components/StarWarsCharacter.js
@@ -1,23 +1,29 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 export default function StarWarsCharacter({ name, birthYear, height, gender }) {
   return (
-    <li>
-      <h3>{name}</h3>
+    <li className="character">
+      <h3 className="character-name">{name}</h3>
       <h4>General Information</h4>
       <ul>
-        <li>Born: {birthYear}</li>
-        <li>Height: {height}</li>
-        <li>Gender: {gender}</li>
+        <li>
+          <span className="bold">Born:</span> {birthYear}
+        </li>
+        <li>
+          <span className="bold">Height:</span> {height}
+        </li>
+        <li>
+          <span className="bold">Gender:</span> {gender}
+        </li>
       </ul>
     </li>
-  )
+  );
 }
 
 StarWarsCharacter.propTypes = {
   name: PropTypes.string.isRequired,
   birthYear: PropTypes.string.isRequired,
   height: PropTypes.string.isRequired,
-  gender: PropTypes.string.isRequired,
-}
+  gender: PropTypes.string.isRequired
+};
